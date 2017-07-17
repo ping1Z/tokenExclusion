61634ed985e54e9759b9f58ecda146022d5d566b
ping1Z <zhongyp.design@gmail.com>
2017-07-17 15:35:20 -0500

    init-project

---
+++ src/MEServiceStatus.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ping1zhong on 7/17/17.
+ */
+public enum  MEServiceStatus {
+    IDLE,
+    ASKED,
+    IN_CS,
+    CLOSE
+}
---
+++ src/MutualExclusionApp.java
@@ -0,0 +1,5 @@
+/**
+ * Created by ping1zhong on 7/17/17.
+ */
+public class MutualExclusionApp {
+}
