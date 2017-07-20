9effa8ce3bf8ff3f7282721b8d151c8f820e973b
lidaxia2017 <mr.liwei1991@gmail.com>
2017-07-19 19:22:39 -0500

    handle event and process message

--- src/MEServerThread.java
+++ src/MEServerThread.java
@@ -7,15 +7,14 @@ import java.util.List;
 public class MEServerThread extends Thread{;
     private int port;
     private List<MEServerEventListener> listeners;
+
     public MEServerThread(int port) {
         super("GCServerThread");
         this.port = port;
-
-
     }
 
-    public void registerEventListenser(MEServerEventListener listener){
-        if(listeners == null){
+    public void registerEventListenser(MEServerEventListener listener) {
+        if(listeners == null) {
             listeners = new ArrayList<MEServerEventListener>();
         }
         listeners.add(listener);
@@ -26,46 +25,30 @@ public class MEServerThread extends Thread{;
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             postServerStarted();
 
-            Socket socket = serverSocket.accept();
+            while (true) {
+                Socket socket = serverSocket.accept();
 
                 try (
                         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                        BufferedReader in = new BufferedReader(
-                                new InputStreamReader(
-                                        socket.getInputStream()));
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 ) {
                     String inputLine;
-                    MEMsg msg = new MEMsg();
                     MELogger.Debug("\n=== Received Msg Start ===");
                     while ((inputLine = in.readLine()) != null) {
-                        if (inputLine.equals(""))
-                            break;
+                        MEMsg msg = new MEMsg(inputLine);
                         MELogger.Debug(inputLine);
-                        String[] kv = inputLine.trim().split(":",2);
-                        if(kv.length<2){
-                            continue;
-                        }
-
-                        if(kv[0].equals("src-id")){
-                            msg.setId(Integer.parseInt(kv[1].trim()));
-                        }else if(kv[0].equals("src-host")){
-                            msg.setHost(kv[1].trim());
-                        }else if(kv[0].equals("src-port")) {
-                            msg.setPort(Integer.parseInt(kv[1].trim()));
-                        }else if(kv[0].equals("content")){
-                            msg.setContent(kv[1].trim());
-                        }
+                        onReceiveMsg(msg);
                     }
                     out.println("200");
                     socket.close();
                     MELogger.Debug("Socket closed.");
-                    onReceiveMsg(msg);
                 } catch (IOException e) {
                     StringWriter sw = new StringWriter();
                     PrintWriter pw = new PrintWriter(sw);
                     e.printStackTrace(pw);
                     MELogger.Error(sw.toString());
                 }
+            }
         } catch (IOException e) {
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
--- src/MutualExclusionService.java
+++ src/MutualExclusionService.java
@@ -1,56 +1,127 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class MutualExclusionService{
 
     private MENodeInfo local;
     private MENodeInfo holder;
-    private MEServiceStatus status;
-    private LinkedList<MEMsg> queue;
+    private boolean ASKED = false;
+    public boolean inCS = false;
+    private HashMap<Integer, MENodeInfo> neighbours;
+    private LinkedList<Integer> queue = new LinkedList<>();
 
     private MEServerThread server;
 
-    public MutualExclusionService(MENodeInfo local, MENodeInfo holder){
+    public MutualExclusionService(MENodeInfo local, MENodeInfo holder, HashMap<Integer, MENodeInfo> neighbours) {
         this.local = local;
         this.holder = holder;
-        status = MEServiceStatus.CLOSE;
-        queue = new LinkedList<>();
+        this.neighbours = neighbours;
     }
 
-    public void start(){
+    public void start() {
         startServer();
     }
 
-    public  void startServer(){
-        if(this.server!=null){
+    public  void startServer() {
+        if(this.server != null) {
             MELogger.Info("MEServer is running already.");
             return;
         }
         this.server = new MEServerThread(this.local.getPort());
 
-        this.server.registerEventListenser(new MEServerEventListener(){
+        this.server.registerEventListenser(new MEServerEventListener() {
 
             @Override
-            public void postServerStarted(){
+            public void postServerStarted() {
                 MELogger.Info("postServerStarted");
-                setStatus(MEServiceStatus.IDLE);
             }
 
             @Override
             public void onReceiveMsg(MEMsg msg) {
-                MELogger.Info("onReceiveMsg");
+                MELogger.Info("onReceiveMsg: " + msg.getContent() + " " + msg.getId());
                 processMsg(msg);
             }
         });
 
         this.server.start();
     }
-    private void setStatus(MEServiceStatus s){
-        this.status = s;
+    private void makeRequest() {
+        if (holder.getId() != local.getId() && !queue.isEmpty() && !ASKED) {
+            sendRequest();
+            ASKED = true;
+        }
     }
 
-    private void processMsg(MEMsg msg){}
-    private void sendMsg(MEMsg msg){}
-    public void csEnter(){}
-    public void csLeave(){}
+    private void assignToken() {
+        if (holder.getId() == local.getId() && !inCS && !queue.isEmpty()) {
+            int requestId = queue.poll();
+            if (requestId != local.getId()) {
+                sendToken(requestId);
+                holder = neighbours.get(requestId);
+                ASKED = false;
+            } else {
+                inCS = true;
+            }
+        }
+    }
 
+    public void processMsg(MEMsg msg) {
+        if (msg.getContent().equals("REQUEST")) {
+            queue.add(msg.getId());
+        } else {
+            holder = local;
+        }
+        assignToken();
+        makeRequest();
+    }
+
+    public void csEnter() {
+        MELogger.Info("Request %d", local.getId());
+        queue.add(local.getId());
+        assignToken();
+        makeRequest();
+    }
+
+    public void csLeave() {
+        inCS = false;
+        assignToken();
+        makeRequest();
+    }
+
+    private void sendRequest() {
+        try
+        {
+            String msg = "REQUEST|" + local.getId();
+            Socket clientSocket = new Socket(holder.getHost(), holder.getPort());
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            out.write(msg);
+            out.close();
+            MELogger.Info("Send Request to %d", holder.getId());
+            clientSocket.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendToken(int requestId) {
+        try
+        {
+            String msg = "TOKEN|" + local.getId();
+            MENodeInfo sendTo = neighbours.get(requestId);
+            Socket clientSocket = new Socket(sendTo.getHost(), sendTo.getPort());
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            out.write(msg);
+            MELogger.Info("Send Token to %d", sendTo.getId());
+            out.close();
+            clientSocket.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
